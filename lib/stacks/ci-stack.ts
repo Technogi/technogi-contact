81381aa35e1fac8bb0fcefbb33c53b3a6efacc02
Carlos Hernandez <carlos@technogi.com.mx>
2023-03-07 20:27:18 -0600

    ci test 8

--- lib/stacks/ci-stack.ts
+++ lib/stacks/ci-stack.ts
@@ -38,6 +38,86 @@ export class CiStack extends cdk.Stack {
       ]
     })
 
+
+    const buildProject = new codebuild.PipelineProject(this, 'build-project', {
+      projectName: `${applicationName}-${stage}`,
+      role: this.createBuildRole(applicationName, stage),
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.AMAZON_LINUX_2_4
+      },
+      environmentVariables: {
+        AWS_ACCOUNT: { value: this.account },
+        AWS_REGION: { value: this.region },
+        APPLICATION: { value: getEnv('APPLICATION') },
+        STAGE: { value: getEnv('STAGE') },
+        PIPE_DRIVE_API_KEY: {
+          value: getEnv('PIPE_DRIVE_API_KEY'),
+          type: codebuild.BuildEnvironmentVariableType.PLAINTEXT
+        },
+        PIPE_DRIVE_API_URL: { value: getEnv('PIPE_DRIVE_API_URL') },
+        GITHUB_REPO: { value: getEnv('GITHUB_REPO') },
+        GITHUB_OWNER: { value: getEnv('GITHUB_OWNER') },
+        CODESTAR_CONNECTION_ARN: { value: getEnv('CODESTAR_CONNECTION_ARN') },
+      },
+      buildSpec: codebuild.BuildSpec.fromObject({
+        version: '0.2',
+        phases: {
+          install: { commands: ['n 16', 'npm install'] },
+          build: { commands: [`npm run cdk deploy -- ${ApplicationStackName} --require-approval never`] }
+        },
+        // artifacts: {
+        //   'base-directory': 'dist',
+        //   files: [
+        //     `${StackName}.template.json`
+        //   ]
+        // }
+      })
+    })
+
+
+    const buildAction = new actions.CodeBuildAction({
+      actionName: 'build',
+      project: buildProject,
+      input: sourceOutput,
+      //outputs: [cdkBuildOutput]
+    })
+
+    new cdk.CfnOutput(this, 'RoleB', {
+      value: buildProject?.role?.roleName || 'na'
+    })
+
+    new cdk.CfnOutput(this, 'RoleA', {
+      value: buildAction.actionProperties?.role?.roleName || 'na'
+    })
+
+    new cdk.CfnOutput(this, 'RoleP', {
+      value: pipeline.role?.roleName
+    })
+
+    pipeline.addStage({
+      stageName: 'Deploy',
+      actions: [buildAction]
+    })
+
+    // pipeline.addStage({
+    //   stageName: 'Deploy',
+    //   actions: [
+    //     new actions.CloudFormationCreateUpdateStackAction({
+    //       actionName: 'deploy',
+    //       templatePath: cdkBuildOutput.atPath(`${StackName}.template.json`),
+    //       stackName: StackName,
+    //       adminPermissions: true,
+    //       parameterOverrides: {
+    //         env: stage,
+    //       },
+    //       extraInputs: [sourceOutput],
+    //     }),
+    //   ]
+    // })
+
+  }
+
+  createBuildRole(applicationName: string, stage: string) {
     const deployRole = new iam.Role(this, 'role', {
       assumedBy: new iam.ServicePrincipal('codebuild.amazonaws.com'),
       roleName: `${applicationName}-ci-${stage}`,
@@ -72,70 +152,58 @@ export class CiStack extends cdk.Stack {
         ],
       }))
 
-    const buildProject = new codebuild.PipelineProject(this, 'build-project', {
-      projectName: `${applicationName}-${stage}`,
-      role: deployRole,
-      environment: {
-        buildImage: codebuild.LinuxBuildImage.AMAZON_LINUX_2_4
-      },
-      environmentVariables: {
-        AWS_ACCOUNT: { value: this.account },
-        AWS_REGION: { value: this.region },
-        APPLICATION: { value: getEnv('APPLICATION') },
-        STAGE: { value: getEnv('STAGE') },
-        PIPE_DRIVE_API_KEY: {
-          value: getEnv('PIPE_DRIVE_API_KEY'),
-          type: codebuild.BuildEnvironmentVariableType.PLAINTEXT
-        },
-        PIPE_DRIVE_API_URL: { value: getEnv('PIPE_DRIVE_API_URL') },
-        GITHUB_REPO: { value: getEnv('GITHUB_REPO') },
-        GITHUB_OWNER: { value: getEnv('GITHUB_OWNER') },
-        CODESTAR_CONNECTION_ARN: { value: getEnv('CODESTAR_CONNECTION_ARN') },
-      },
-      buildSpec: codebuild.BuildSpec.fromObject({
-        version: '0.2',
-        phases: {
-          install: { commands: ['n 16', 'npm install'] },
-          build: { commands: [`npm run cdk deploy -- ${ApplicationStackName} --require-approval never`] }
-        },
-        // artifacts: {
-        //   'base-directory': 'dist',
-        //   files: [
-        //     `${StackName}.template.json`
-        //   ]
-        // }
-      })
-    })
+    deployRole.addToPrincipalPolicy(new iam.PolicyStatement({
+      actions: [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      resources: [
+        `arn:aws:logs:${this.region}:${this.account}:log-group:/aws/codebuild/${applicationName}-${stage}:*`,
+        `arn:aws:logs:${this.region}:${this.account}:log-group:/aws/codebuild/${applicationName}-${stage}`
+      ]
+    }))
 
-    const buildAction = new actions.CodeBuildAction({
-      actionName: 'build',
-      project: buildProject,
-      input: sourceOutput,
-      //outputs: [cdkBuildOutput]
-    })
 
-    pipeline.addStage({
-      stageName: 'Deploy',
-      actions: [buildAction]
-    })
+    deployRole.addToPrincipalPolicy(new iam.PolicyStatement({
+      actions: [
+        "codebuild:BatchPutCodeCoverages",
+        "codebuild:BatchPutTestCases",
+        "codebuild:CreateReport",
+        "codebuild:CreateReportGroup",
+        "codebuild:UpdateReport"
+      ],
+      resources: [
+        `arn:aws:codebuild:${this.region}:${this.account}:report-group/${applicationName}-${stage}-*`
+      ]
+    }))
 
-    //pipeline.notifyOn('e', new NotificationRule(this,'rr',{ }))
+    deployRole.addToPrincipalPolicy(new iam.PolicyStatement({
+      actions: [
+        "s3:GetBucket*",
+        "s3:GetObject*",
+        "s3:List*"
+      ],
+      resources: [
+        `arn:aws:s3:::${applicationName}-ci-${stage}-*`,
+        `arn:aws:s3:::${applicationName}-ci-${stage}-*/*`
+      ]
+    }))
 
-    // pipeline.addStage({
-    //   stageName: 'Deploy',
-    //   actions: [
-    //     new actions.CloudFormationCreateUpdateStackAction({
-    //       actionName: 'deploy',
-    //       templatePath: cdkBuildOutput.atPath(`${StackName}.template.json`),
-    //       stackName: StackName,
-    //       adminPermissions: true,
-    //       parameterOverrides: {
-    //         env: stage,
-    //       },
-    //       extraInputs: [sourceOutput],
-    //     }),
-    //   ]
-    // })
+    deployRole.addToPrincipalPolicy(new iam.PolicyStatement({
+      actions: [
+        "kms:Decrypt",
+        "kms:DescribeKey",
+        "kms:Encrypt",
+        "kms:GenerateDataKey*",
+        "kms:ReEncrypt*"
+      ],
+      resources: [
+        `arn:aws:kms:${this.region}:${this.account}:key/*`
+      ]
+    }))
+
+    return deployRole
 
   }
 
